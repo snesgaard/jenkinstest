57151398cfe58aefa6a2bc36467a7e3d916896d1
rufusGIT <rufus.blas@vrinno.com>
2018-04-16 13:56:59 +0200

    Testing files added

---
+++ src/stuff.py
@@ -0,0 +1,8 @@
+def add(a, b):
+    return a + b
+
+def dot(a, b):
+    return a * b
+
+def div(a, b):
+    return dot(a, b)
---
+++ test/test_stuff.py
@@ -0,0 +1,14 @@
+import sys
+sys.path.append('src/')
+import stuff
+
+def test_add():
+    assert stuff.add(1, 2) == 3
+    assert stuff.add(10, 1) == 11
+    assert stuff.add(5, 6) == 11
+    assert stuff.add(6, 6) == 12
+
+def test_dot():
+    assert stuff.dot(1, 1) == 1
+    assert stuff.dot(5, 1) == 5
+    assert stuff.dot(3, 5) == 15
